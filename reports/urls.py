ba813b8c2ea421817ceb714c44f6b9de87b02602
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-10-23 12:19:42 +0200

    much better progress

--- reports/urls.py
+++ reports/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-from django.contrib.auth import views as auth_views
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
