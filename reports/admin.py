885ade3c686eee0e8da60b7c3d44f3b1806354ff
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-10-31 12:29:49 +0200

    internal messages

--- reports/admin.py
+++ reports/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, IncidentType, Symptom, HealthReport, Location, Alert
+from .models import *
 
 admin.site.register(UserProfile)
 admin.site.register(IncidentType)
@@ -7,3 +7,4 @@ admin.site.register(Symptom)
 admin.site.register(HealthReport)
 admin.site.register(Location)
 admin.site.register(Alert)
+admin.site.register(SystemContact)
